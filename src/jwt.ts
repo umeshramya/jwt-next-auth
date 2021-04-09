2565f4683c2a285875c8218977dbbbfc3d0ba749
umeshramya <umeshbilagi@gmail.com>
2021-04-09 13:53:42 +0530

    woorking

--- src/jwt.ts
+++ src/jwt.ts
@@ -2,20 +2,22 @@
 
 import jwt from "jsonwebtoken"
 import Cookies from "cookies"
-import {NextApiResponse, NextApiRequest} from "next"
+import {NextApiResponse, NextApiRequest, NextApiHandler} from "next"
 
 
 const SECRET_AUTH = process.env.SECRET_AUTH || "ramya darling"
 
-const jwtSign = (payload:{}, res:NextApiResponse)=>{
+const jwtSign = (payload:{},req:NextApiRequest, res:NextApiResponse)=>{
     // this fun is for only creating login jwt 
     
         return new Promise((resolve, reject)=>{
             jwt.sign(payload, SECRET_AUTH, { expiresIn: '1d' }, (err, token)=>{
                 if(err){
                     reject(err)
+                }else if (token === undefined){
+                    reject("token is undefined")
                 }else{
-                    setJwtTokenCookie(token, res);
+                    setJwtTokenCookie(token ,req, res);
                     resolve(token)
                 }
             });
@@ -41,15 +43,14 @@ const jwtTokenCreate = (payload:{})=>{
         
     })
 
-
-
-
 }
+
+
 
 const jwtverify = (token:string) =>{
     // this verify the token
     return new Promise((resolve, reject)=>{
-        jwt.verify(token, SECRET_AUTH, (err, decoded)=>{
+        jwt.verify(token, SECRET_AUTH, (err, decoded:any)=>{
             if(err){
                 reject(false)
             }else{
@@ -61,40 +62,43 @@ const jwtverify = (token:string) =>{
 }
 
 
-const IsPageLogged = (ctx)=>{
-    return new Promise((resolve, reject)=>{
-        let {token }= cookie.parse(ctx.req.headers.cookie);
-        jwtverify(token).then(result =>{resolve(result)}).catch(err =>{reject(err)});
+// const IsPageLogged = (ctx)=>{
+//     return new Promise((resolve, reject)=>{
+//         let {token }= cookie.parse(ctx.req.headers.cookie);
+//         jwtverify(token).then(result =>{resolve(result)}).catch(err =>{reject(err)});
  
             
-    })
-}
+//     })
+// }
 
 
 
-const validateUser = async(req, res, next)=>{
+const validateUser = async(req:NextApiRequest, res:NextApiResponse, next:Function)=>{
     // this is middle were to validate user cretanay=tials
     // return false if not logged in 
-    try {
-        let {token }= cookie.parse(req.headers.cookie);
-       const result =  await jwtverify(token).then(result => result)
+  
+        let cookies = new Cookies(req, res);
+        let token = cookies.get("token");
+        if(token === undefined){
+            throw new Error().message="Invalid Login";
+        }else{
+           req.body.pemrAuth = await jwtverify(token).then(result => result)
 
-       req.pemrAuth = result
+        }
+     
         next();
-    } catch (error) {
-        console.log(error)
-        res.sttus(500).send(error)
-    }
+
     
 }
 
 
-const setJwtTokenCookie = (token, res)=>{
-     res.cookie("token", token ,{
+const setJwtTokenCookie = (token:string, req:NextApiRequest, res:NextApiResponse)=>{
+    let cookies = new Cookies(req, res);
+    cookies.set("token", token, {
         httpOnly : true,
         sameSite : true
-     });
+    })
     
 }
 
-module.exports = {jwtSign, jwtverify, IsPageLogged, validateUser, jwtTokenCreate}
+module.exports = {jwtSign, jwtverify,  validateUser, jwtTokenCreate}
