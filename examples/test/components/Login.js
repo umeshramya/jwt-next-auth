642c321dd6883c67f8d85ce0361069facd8da971
umeshramya <umeshbilagi@gmail.com>
2021-04-10 14:14:46 +0530

    v1.0.1

--- examples/test/components/Login.js
+++ examples/test/components/Login.js
@@ -1,18 +1,40 @@
-import React from 'react'
-import {Container, Row, Col,  FormGroup, Label, Input} from "reactstrap"
-import {FormSubmit} from "reactstrap-react-lib"
+import React, {useState} from 'react'
+import { Container, Row, Col, FormGroup, Label, Input } from "reactstrap"
+import { FormSubmit } from "reactstrap-react-lib"
 
 function Login() {
+    let initialState = {
+        username: "", password: ""
+    }
+    const [InputObj, setInputObj] = useState(initialState)
     return (
         <>
             <Row>
                 <Col>
                     <FormSubmit
+                        Inputs={
+                            <>
+                            <FormGroup>
+                                <Label>User Name</Label>
+                                <Input type="text" value={InputObj.username} onChange={e => setInputObj({ ...InputObj, username: e.target.value })} required={true} />
+                            </FormGroup>
+                            <FormGroup>
+                            <Label>password</Label>
+                            <Input type="password" value={InputObj.password} onChange={e => setInputObj({ ...InputObj, password: e.target.value })} required={true} />
+                        </FormGroup>
+                        </>
+                        }
+
+                        curUri="/api/login"
+                        curObj = {InputObj}
+                        reset = {()=>setInputObj(initialState)}
+                        onSuccess={(r)=>console.log(r.data.mes)}
+                        onError = {(er)=>console.log(er.response.data)}
 
                     />
                 </Col>
             </Row>
-            
+
         </>
     )
 }
---
+++ examples/test/pages/api/login.js
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {jwtSign} from "jwt-next-auth"
+
+const route = async(req, res) => {
+  try {
+    let result = await jwtSign(req.body, req , res).then(res=>res);
+    console.log(result)
+    res.status(200).json({mes : result})
+  } catch (error) {
+    res.status(500).send(error)
+  }
+}
+
+
+
+
+
+export default route;
