5db28d523f8c3404366e31827fe09aad86b4502d
umeshramya <umeshbilagi@gmail.com>
2024-11-23 21:52:37 +0530

    working with dev

--- test.js
+++ test.js
@@ -1,26 +1,12 @@
-// const {encrypt, decrypt} = require("./lib/jwt")
-// // Example usage
-// const originalText = "Hello, world!";
-// const encryptedText = encrypt(originalText);
-// const decryptedText = decrypt(encryptedText);
-
-// console.log("Original Text:", originalText);
-// console.log("Encrypted Text:", encryptedText);
-// console.log("Decrypted Text:", decryptedText);
-
-
-const {asymmetricEncrypt, asymmetricDecrypt} = require("./lib/asymtricEncrypt")
+const {encrypt, decrypt} = require("./lib/jwt")
 // Example usage
-// Example usage
-const message = 'Hello, secure UHI!';
-console.log('Original Message:', message);
+const originalText = "Hello, world!";
+const encryptedText = encrypt(originalText);
+const decryptedText = decrypt(encryptedText);
 
-try {
-  const encryptedMessage = asymmetricEncrypt(message);
-  console.log('Encrypted Message:', encryptedMessage);
+console.log("Original Text:", originalText);
+console.log("Encrypted Text:", encryptedText);
+console.log("Decrypted Text:", decryptedText);
 
-  const decryptedMessage = asymmetricDecrypt(encryptedMessage);
-  console.log('Decrypted Message:', decryptedMessage);
-} catch (error) {
-  console.error('Error during encryption/decryption:', error.message);
-}
+
+
